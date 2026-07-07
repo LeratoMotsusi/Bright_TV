fcfabe54ba423807e3f8b62a34394efb9a1c67b8
LeratoMotsusi <leemotsusi@gmail.com>
2026-07-07 18:54:44 +0000

    Gender checks

--- Bright_TV Coding.sql
+++ Bright_TV Coding.sql
@@ -1,8 +1,27 @@
 -- Databricks notebook source
-SELECT *
-FROM bright_tv.data.user_profiles;
-
-SELECT *
-FROM bright_tv.data.viewerships;
-
+---Telling Databricks to use the "Bright_TV" catalog and the "data" schema
 USE bright_tv.data;
+
+
+---Running the full tables
+SELECT *
+FROM user_profiles;
+
+SELECT *
+FROM viewerships;
+
+--- Gender checks
+SELECT DISTINCT Gender
+FROM user_profiles;
+
+
+SELECT DISTINCT
+    CASE
+        WHEN Gender = 'None' THEN 'unknown' ---Replaces the value "None" with "Unkown"
+        WHEN Gender = ' ' THEN 'unknown' ---Replaces the blanks with "Unkown"
+    ELSE Gender
+    END AS Gender_clean
+FROM user_profiles;
+
+
+
