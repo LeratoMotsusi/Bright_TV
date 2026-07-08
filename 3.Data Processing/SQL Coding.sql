9513c5cc8c247cf7657b25bf2911a00b5e081817
Lerato Motsusi <leemotsusi@gmail.com>
2026-07-08 19:51:13 +0200

    Implement gender and race data cleaning queries
    Added SQL queries for gender and race checks with data cleaning.

--- 3.Data Processing/SQL Coding.sql
+++ 3.Data Processing/SQL Coding.sql
@@ -1,1 +1,40 @@
+---Telling Databricks to use the "Bright_TV" catalog and the "data" schema
+USE bright_tv.data;
+
+
+---Running the full tables
+SELECT *
+FROM user_profiles;
+
+SELECT *
+FROM viewerships;
+
+--- Gender checks
+SELECT DISTINCT Gender
+FROM user_profiles;
+
+
+SELECT DISTINCT
+    CASE
+        WHEN Gender = 'None' THEN 'unknown' ---Replaces the value "None" with "Unkown"
+        WHEN Gender = ' ' THEN 'unknown' ---Replaces the blanks with "Unkown"
+    ELSE Gender
+    END AS Gender_clean
+FROM user_profiles;
+
+
+
+---Race Checks
+SELECT DISTINCT Race
+FROM user_profiles;
+
+SELECT DISTINCT
+    CASE 
+        WHEN Race = 'other' THEN 'unknown'
+        WHEN Race = 'None' THEN 'unknown'
+    ELSE Race
+    END AS Race_clean
+FROM user_profiles;
+
+
 
